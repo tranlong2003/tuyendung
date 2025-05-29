8844315f8631d722ac77b046e55d002638fa31a7
tranlong2003 <139291042+tranlong2003@users.noreply.github.com>
2025-05-29 14:55:16 +0700

    Update config.php

--- config.php
+++ config.php
@@ -9,22 +9,3 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Kết nối thất bại: " . $conn->connect_error);
 }
-
-// Nhận dữ liệu từ form
-$user = $_POST['username'];
-$pass = $_POST['password'];
-
-// Lưu vào CSDL
-$sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", $user, $pass);
-
-if ($stmt->execute()) {
-  echo "Đăng nhập thành công và lưu dữ liệu!";
-} else {
-  echo "Lỗi: " . $stmt->error;
-}
-
-$stmt->close();
-$conn->close();
-?>
